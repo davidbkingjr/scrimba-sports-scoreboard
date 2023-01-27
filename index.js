21e645843e8e56c1109f1033767c9ae4032469d9
David King <117994855+davidbkingjr@users.noreply.github.com>
2023-01-26 20:26:48 -0500

    Initial upload of the Sports Scoreboard app

---
+++ index.js
@@ -0,0 +1,43 @@
+ let homeNum = 0
+ let awayNum = 0 
+ let homeScore = document.getElementById("home-el") 
+ let awayScore = document.getElementById("away-el")
+ 
+// Home Score
+ function addOneHome() {
+     homeNum += 1
+     homeScore.textContent = homeNum
+ }
+ 
+ function addTwoHome() {
+     homeNum += 2
+     homeScore.textContent = homeNum
+ }
+ 
+ function addThreeHome() {
+     homeNum += 3
+     homeScore.textContent = homeNum
+ }
+ 
+ // Away Score 
+  function addOneAway() {
+     awayNum += 1
+     awayScore.textContent = awayNum
+ }
+ 
+ function addTwoAway() {
+     awayNum += 2
+     awayScore.textContent = awayNum
+ }
+ 
+ function addThreeAway() {
+     awayNum += 3
+     awayScore.textContent = awayNum
+ }
+ 
+ // New Game
+ 
+ function newGame() {
+     homeScore.textContent = 0
+     awayScore.textContent = 0
+ }
